449e63672310780e968d57be8a9fb4d12e9cc0ea
İsmail Akbudak <akbudak.ismail@yandex.com>
2016-03-31 01:00:11 +0300

    complete distance and duration matrix

--- play_area.rb
+++ play_area.rb
@@ -1,16 +1,11 @@
 require 'json'
+require './helpers'
 
-cities_json = "./data/cities.json"
+distance_result_file_name 	= "./distance_result.txt"
+duration_result_file_name 	= "./duration_result.txt"
+matrix_file_name 			= "./data/matrix.json"
+matrix_file 				= File.read(matrix_file_name)
+matrix_json 				= JSON.parse(matrix_file)	
 
-file = File.read(cities_json)
-cities = JSON.parse(file)
-
-index = 1
-cities.each do |city|
-  city['id'] = index
-  index += 1 
-end
-
-File.open(cities_json, 'w') do |f| 
-	f.write JSON.pretty_generate(cities)
-end
+create_matrix "distance", matrix_json, distance_result_file_name
+create_matrix "duration", matrix_json, duration_result_file_name
