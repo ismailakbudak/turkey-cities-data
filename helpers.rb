b3156f4d5aee67cd40adb65162e3034d9a3b12e1
ismail akbudak <akbudak.ismail@yandex.com>
2016-04-03 19:02:43 +0300

    complete matrix result view

--- helpers.rb
+++ helpers.rb
@@ -54,3 +54,48 @@ def create_matrix field_name, matrix_json, result_file_name
 		write_result_file(val, result_file_name)  
 	end
 end
+
+def create_matrix_for_float field_name, matrix_json, result_file_name
+	clear_result_file result_file_name
+	
+	from_city_ids = matrix_json.map{|x| x['from_id']}.uniq.sort
+	to_city_ids = matrix_json.map{|x| x['to_id']}.uniq.sort
+
+	max_length = 15
+	(1..max_length).each do |i|
+		val = "%19s" % [" "]
+		to_city_ids.each do |to_id|
+			city = matrix_json.select{|c| c['to_id'] == to_id}.first
+			index = max_length - i + 1
+			name_length = city["to_name"].length 
+			if max_length - i < name_length 
+				k = name_length - index
+				val += "        %s    " % [ city["to_name"][k] ]	
+			else	
+				val += "        %s    " % [ " " ]
+			end
+		end
+		write_result_file(val, result_file_name)  
+	end 
+
+	val = "%19s" % [" "]
+	to_city_ids.each do |to_id|
+		city = matrix_json.select{|c| c['to_id'] == to_id}.first
+		# val += "%15s %02d" % [ city["to_name"], to_id ]
+		val += "        %02d   " % [ to_id ]
+	end
+
+	write_result_file(val, result_file_name)  
+
+	from_city_ids.each do |from_id|
+		city = matrix_json.select{|c| c['from_id'] == from_id}.first
+
+		val = "%15s %02d" % [ city["from_name"], from_id ]
+		
+		to_city_ids.each do |to_id|
+			matrix_row_city = matrix_json.select{|x| x['from_id'] == from_id and x['to_id'] == to_id}.first
+			val += "%#13s" % [ ( "%.3f" % matrix_row_city[field_name]).to_s ]
+		end
+		write_result_file(val, result_file_name)  
+	end
+end
--- play_area.rb
+++ play_area.rb
@@ -1,11 +1,16 @@
 require 'json'
 require './helpers'
 
-distance_result_file_name 	= "./distance_result.txt"
-duration_result_file_name 	= "./duration_result.txt"
+distance_result_file_name 	= "./data/distance_result.txt"
+duration_result_file_name 	= "./data/duration_result.txt"
+euclidean_result_file_name 	= "./data/euclidean_result.txt"
+circuity_result_file_name 	= "./data/circuity_result.txt"
+
 matrix_file_name 			= "./data/matrix.json"
 matrix_file 				= File.read(matrix_file_name)
 matrix_json 				= JSON.parse(matrix_file)	
 
 create_matrix "distance", matrix_json, distance_result_file_name
 create_matrix "duration", matrix_json, duration_result_file_name
+create_matrix_for_float "euclidean_distance", matrix_json, euclidean_result_file_name
+create_matrix_for_float "circuity_factor", matrix_json, circuity_result_file_name
