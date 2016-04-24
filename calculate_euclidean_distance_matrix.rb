d67b095db7d368d172e2ea75fae811670fa03ea0
ismail akbudak <akbudak.ismail@yandex.com>
2016-04-24 16:08:09 +0300

    calculate euclidean distance for matrix json

--- calculate_euclidean_distance_matrix.rb
+++ calculate_euclidean_distance_matrix.rb
@@ -1,26 +1,33 @@
 require 'json'
 
-cities_json = "./data/cities.json"
-matrix_json = "./data/matrix.json"
+def calculate(locations_json, locations_dd_matrix_json)
+	locations = JSON.parse(File.read(locations_json))
+	matrix 		= JSON.parse(File.read(locations_dd_matrix_json))
 
-cities = JSON.parse(File.read(cities_json))
-matrix = JSON.parse(File.read(matrix_json))
+	matrix.each do |value|
+		from 	= locations.select{|i| i['id'] == value['from_id'] }.first
+		to 		= locations.select{|i| i['id'] == value['to_id'] }.first
+		if from.nil? or to.nil?
+			puts value.inspect
+		else
+			x = (from['lattitude'].to_f - to['lattitude'].to_f) ** 2
+			y = (from['longitude'].to_f - to['longitude'].to_f) ** 2
+			euclidean_distance = Math.sqrt( x + y )
+			value['euclidean_distance'] = euclidean_distance
+		end
+	end
 
-
-matrix.each do |value|
-	from = cities.select{|i| i["id"] == value["from_id"] }.first
-	to = cities.select{|i| i["id"] == value["to_id"] }.first
-	if from.nil? or to.nil?
-		puts value.inspect
-	else
-		x = (from["lattitude"].to_f - to["lattitude"].to_f) ** 2
-		y = (from["longitude"].to_f - to["longitude"].to_f) ** 2 
-		euclidean_distance = Math.sqrt( x + y )
-		value["euclidean_distance"] = euclidean_distance
+	File.open(locations_dd_matrix_json, 'w') do |f|
+		f.write JSON.pretty_generate(matrix)
 	end
 end
 
-
-File.open(matrix_json, 'w') do |f| 
-	f.write JSON.pretty_generate(matrix)
+if __FILE__ == $0
+	if ARGV.length == 1
+		locations_json = "./data/#{ARGV[0]}.json"
+		locations_dd_matrix_json = "./data/#{ARGV[0]}-dd-matrix.json"
+		calculate locations_json, locations_dd_matrix_json
+	else
+		puts 'You should enter yml file name existing in data directory'
+	end
 end
