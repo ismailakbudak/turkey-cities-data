ac590562bbb008b25d167b4c5aa4677ffaa02645
ismail akbudak <akbudak.ismail@yandex.com>
2016-04-24 16:11:50 +0300

    calculate circuity factor for imbros

--- calculate_circuity_factor_matrix.rb
+++ calculate_circuity_factor_matrix.rb
@@ -1,20 +1,29 @@
 require 'json'
- 
-matrix_json = "./data/matrix.json"
-matrix = JSON.parse(File.read(matrix_json))
 
-matrix.each do |value|
-	distance = value["distance"].to_f
-	euclidean_distance =  value["euclidean_distance"].to_f
-	if distance == 0
-		circuity_factor = 0
-	else
-		circuity_factor = distance / euclidean_distance
-	end	
-	value["circuity_factor"] = circuity_factor
+def calculate matrix_json
+	matrix = JSON.parse(File.read(matrix_json))
+
+	matrix.each do |value|
+		distance 						= value['distance'].to_f
+		euclidean_distance 	= value['euclidean_distance'].to_f
+		if distance == 0
+			circuity_factor = 0
+		else
+			circuity_factor = distance / euclidean_distance
+		end
+		value['circuity_factor'] = circuity_factor
+	end
+
+	File.open(matrix_json, 'w') do |f|
+		f.write JSON.pretty_generate(matrix)
+	end
 end
 
-
-File.open(matrix_json, 'w') do |f| 
-	f.write JSON.pretty_generate(matrix)
+if __FILE__ == $0
+	if ARGV.length == 1
+		locations_dd_matrix_json = "./data/#{ARGV[0]}-dd-matrix.json"
+		calculate locations_dd_matrix_json
+	else
+		puts 'You should enter json file name existing in data directory'
+	end
 end
