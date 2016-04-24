25abfe29a09a3bb512f12d2bb6e4d5b38eeed9ed
ismail akbudak <akbudak.ismail@yandex.com>
2016-04-24 17:26:28 +0300

    complete result file

--- helpers.rb
+++ helpers.rb
@@ -1,120 +1,132 @@
 
-def clear_result_file file
-	File.open(file, 'w') { |f| f.write "" }
+def clear_result_file(file)
+	File.open(file, 'w') { |f| f.write '' }
 end
 
-def write_result_file val, file
-	File.open(file, 'ab+') do |f| 
+def write_result_file(val, file)
+	File.open(file, 'ab+') do |f|
 		f.write val
-		f.write "\n" 
+		f.write "\n"
 	end
 end
 
-def create_matrix field_name, matrix_json, result_file_name
+def create_matrix(field_name, matrix_json, result_file_name)
 	clear_result_file result_file_name
-	
-	from_city_ids = matrix_json.map{|x| x['from_id']}.uniq.sort
-	to_city_ids = matrix_json.map{|x| x['to_id']}.uniq.sort
 
-	max_length = 15
+	from_city_ids = matrix_json.map { |x| x['from_id'] }.uniq.sort
+	to_city_ids = matrix_json.map { |x| x['to_id'] }.uniq.sort
+
+	max_length 			= get_max_name_length(matrix_json, 'to_name')
+	number_distance = 2
+	value_length    = 9
+	blank_distance 	= max_length + number_distance + 1
 	(1..max_length).each do |i|
-		val = "%19s" % [" "]
+		val = "%#{blank_distance}s" % [' ']
 		to_city_ids.each do |to_id|
-			city = matrix_json.select{|c| c['to_id'] == to_id}.first
-			index = max_length - i + 1
-			name_length = city["to_name"].length 
-			if max_length - i < name_length 
+			city 				= matrix_json.select { |c| c['to_id'] == to_id }.first
+			index 			= max_length - i + 1
+			name 				= get_name(city, 'to_name')
+			name_length = name.length
+			if max_length - i < name_length
 				k = name_length - index
-				val += "      %s  " % [ city["to_name"][k] ]	
-			else	
-				val += "      %s  " % [ " " ]
+				val += "%#{value_length - 1}s%s" % [' ', name[k]]
+			else
+				val += "%#{value_length}s" % [' ']
 			end
 		end
-		write_result_file(val, result_file_name)  
-	end 
-
-	val = "%19s" % [" "]
-	to_city_ids.each do |to_id|
-		city = matrix_json.select{|c| c['to_id'] == to_id}.first
-		# val += "%15s %02d" % [ city["to_name"], to_id ]
-		val += "     %02d  " % [ to_id ]
+		write_result_file(val, result_file_name)
 	end
 
-	write_result_file(val, result_file_name)  
+	val = "%#{blank_distance}s" % [' ']
+	to_city_ids.each do |to_id|
+		val += "%#{value_length - number_distance}s%0#{number_distance}d" % [' ', to_id]
+	end
 
+	write_result_file(val, result_file_name)
+
+	# max_length = get_max_name_length(matrix_json, 'from_name')
 	from_city_ids.each do |from_id|
-		city = matrix_json.select{|c| c['from_id'] == from_id}.first
+		city = matrix_json.select { |c| c['from_id'] == from_id }.first
 
-		val = "%15s %02d" % [ city["from_name"], from_id ]
-		
+		val = "%#{max_length}s %0#{number_distance}d" % [get_name(city, 'from_name'), from_id]
+
 		to_city_ids.each do |to_id|
-			matrix_row_city = matrix_json.select{|x| x['from_id'] == from_id and x['to_id'] == to_id}.first
-			val += "%#9d" % [ matrix_row_city[field_name].to_s ]
+			matrix_row_city = matrix_json.select { |x| x['from_id'] == from_id and x['to_id'] == to_id }.first
+			val += "%##{value_length}d" % [matrix_row_city[field_name].to_s]
 		end
-		write_result_file(val, result_file_name)  
+		write_result_file(val, result_file_name)
 	end
 end
 
-def create_matrix_for_float field_name, matrix_json, result_file_name
+def create_matrix_for_float(field_name, matrix_json, result_file_name)
 	clear_result_file result_file_name
-	
-	from_city_ids = matrix_json.map{|x| x['from_id']}.uniq.sort
-	to_city_ids = matrix_json.map{|x| x['to_id']}.uniq.sort
 
-	max_length = 15
+	from_city_ids = matrix_json.map { |x| x['from_id'] }.uniq.sort
+	to_city_ids = matrix_json.map { |x| x['to_id'] }.uniq.sort
+
+	max_length 			= get_max_name_length(matrix_json, 'to_name')
+	number_distance = 2
+	value_length    = 13
+	blank_distance 	= max_length + number_distance + 1
 	(1..max_length).each do |i|
-		val = "%19s" % [" "]
+		val = "%#{blank_distance}s" % [' ']
 		to_city_ids.each do |to_id|
-			city = matrix_json.select{|c| c['to_id'] == to_id}.first
-			index = max_length - i + 1
-			name_length = city["to_name"].length 
-			if max_length - i < name_length 
+			city 				= matrix_json.select { |c| c['to_id'] == to_id }.first
+			index 			= max_length - i + 1
+			name 				= get_name(city, 'to_name')
+			name_length = name.length
+			if max_length - i < name_length
 				k = name_length - index
-				val += "        %s    " % [ city["to_name"][k] ]	
-			else	
-				val += "        %s    " % [ " " ]
+				val += "%#{value_length - 1}s%s" % [' ', name[k]]
+			else
+				val += "%#{value_length}s" % [' ']
 			end
 		end
-		write_result_file(val, result_file_name)  
-	end 
-
-	val = "%19s" % [" "]
-	to_city_ids.each do |to_id|
-		city = matrix_json.select{|c| c['to_id'] == to_id}.first
-		# val += "%15s %02d" % [ city["to_name"], to_id ]
-		val += "        %02d   " % [ to_id ]
+		write_result_file(val, result_file_name)
 	end
 
-	write_result_file(val, result_file_name)  
+	val = "%#{blank_distance}s" % [' ']
+	to_city_ids.each do |to_id|
+		val += "%#{value_length - number_distance}s%0#{number_distance}d" % [' ', to_id]
+	end
+
+	write_result_file(val, result_file_name)
 
 	from_city_ids.each do |from_id|
-		city = matrix_json.select{|c| c['from_id'] == from_id}.first
+		city = matrix_json.select { |c| c['from_id'] == from_id }.first
 
-		val = "%15s %02d" % [ city["from_name"], from_id ]
-		
+		val = "%#{max_length}s %0#{number_distance}d" % [get_name(city, 'from_name'), from_id]
+
 		to_city_ids.each do |to_id|
-			matrix_row_city = matrix_json.select{|x| x['from_id'] == from_id and x['to_id'] == to_id}.first
-			val += "%#13s" % [ ( "%.3f" % matrix_row_city[field_name]).to_s ]
+			matrix_row_city = matrix_json.select { |x| x['from_id'] == from_id and x['to_id'] == to_id }.first
+			val += "%##{value_length}s" % [('%.3f' % matrix_row_city[field_name]).to_s]
 		end
-		write_result_file(val, result_file_name)  
+		write_result_file(val, result_file_name)
 	end
 end
 
-def create_coorditanion cities_json, result_file_name
+def create_coordination(locations_json, result_file_name)
 	clear_result_file result_file_name
+	max_name = get_max_name_length(locations_json)
+	val  = "%#{max_name}s  " % ['Şehir']
+	val += '%12s   ' % ['X(Enlem)']
+	val += '%12s   ' % ['Y(Boylam)']
+	write_result_file(val, result_file_name)
 
-	val  = "%15s  " % ["Şehir"]
-	val += "%12s   " % ["X(Enlem)"]
-	val += "%12s   " % ["Y(Boylam)"]
-	write_result_file(val, result_file_name)  
-
-	cities_json.each do |city|
-
-		val = "%15s %02d" % [ city["name"], city["id"] ]
-		val += "%#13s" % [ ( "%.7f" % city["lattitude"]).to_s ]
-		val += "%#13s" % [ ( "%.7f" % city["longitude"]).to_s ]
-		write_result_file(val, result_file_name)  
-
+	locations_json.each do |city|
+		val = "%#{max_name}s %02d" % [ get_name(city), city['id'] ]
+		val += '%#13s' % [( '%.7f' % city['lattitude']).to_s ]
+		val += '%#13s' % [( '%.7f' % city['longitude']).to_s ]
+		write_result_file(val, result_file_name)
 	end
 
-end		
+end
+
+def get_max_name_length(locations_json, field_name = 'name')
+	location = locations_json.max_by{|b| b[field_name].length }
+	get_name(location, field_name).length
+end
+
+def get_name(location, field_name = 'name')
+	location[field_name].split(',')[0]
+end
--- play_area.rb
+++ play_area.rb
@@ -1,19 +1,35 @@
 require 'json'
 require './helpers'
+require 'fileutils'
 
-distance_result_file_name 			= "./data/distance_result.txt"
-duration_result_file_name 			= "./data/duration_result.txt"
-euclidean_result_file_name 			= "./data/euclidean_result.txt"
-circuity_result_file_name 			= "./data/circuity_result.txt"
-cities_cordination_result_file_name	= "./data/cities_cordination_result.txt"
+def create(parameter)
+  root = "./result/#{parameter}"
+  unless File.directory?(root)
+    FileUtils.mkdir_p(root)
+  end
 
-matrix_file_name 			= "./data/matrix.json"
-cities_file_name 			= "./data/cities.json"
-matrix_json 				= JSON.parse(File.read(matrix_file_name))	
-cities_json 				= JSON.parse(File.read(cities_file_name))	
+  distance_result_file_name 	  = "#{root}/distance.txt"
+  duration_result_file_name 		= "#{root}/duration.txt"
+  euclidean_result_file_name 		= "#{root}/euclidean.txt"
+  circuity_result_file_name 		= "#{root}/circuity.txt"
+  coordination_result_file_name	= "#{root}/coordination.txt"
+  matrix_file_name 			        = "./data/#{parameter}-dd-matrix.json"
+  cities_file_name 			        = "./data/#{parameter}.json"
 
-create_matrix "distance", matrix_json, distance_result_file_name
-create_matrix "duration", matrix_json, duration_result_file_name
-create_matrix_for_float "euclidean_distance", matrix_json, euclidean_result_file_name
-create_matrix_for_float "circuity_factor", matrix_json, circuity_result_file_name
-create_coorditanion cities_json, cities_cordination_result_file_name
+  matrix_json 				= JSON.parse(File.read(matrix_file_name))
+  cities_json 				= JSON.parse(File.read(cities_file_name))
+
+  create_matrix 'distance', matrix_json, distance_result_file_name
+  create_matrix 'duration', matrix_json, duration_result_file_name
+  create_matrix_for_float 'euclidean_distance', matrix_json, euclidean_result_file_name
+  create_matrix_for_float 'circuity_factor', matrix_json, circuity_result_file_name
+  create_coordination cities_json, coordination_result_file_name
+end
+
+if __FILE__ == $0
+  if ARGV.length == 1
+    create ARGV[0]
+  else
+    puts 'You should enter json file name existing in data directory'
+  end
+end
