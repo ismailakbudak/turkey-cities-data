1c64bf3d21870c6f05a7d54c809909cc17355e36
ismail akbudak <akbudak.ismail@yandex.com>
2016-05-15 14:08:16 +0300

    calculate new distance and duration matrix of aegean cities

--- create_distance_duration_matrix_json.rb
+++ create_distance_duration_matrix_json.rb
@@ -7,28 +7,29 @@ def create_distance_duration_matrix_json(locations_json, matrix_json, path_direc
 	locations.each do |location|
 
 		root 	= "#{path_directory}/#{location['id']}"
-		val 	= {'from_name': location['name'], 'to_name': location['name'], 'from_id': location['id'], 'to_id': location['id'], 'distance': 0, 'duration': 0 }
+		val 	= { 'from_id': location['id'], 'to_id': location['id'], 'distance': 0, 'duration': 0 }
 		matrix << val
 
 		locations.select{|i| i['id'] > location['id'] }.each do |location_in|
 
 			file_name = "#{root}/#{parameter}-#{location['id']}-#{location_in['id']}.json"
+			puts file_name
 			routes 		= JSON.parse(File.read(file_name))
 
-			puts distance = routes[0]['legs'][0]['distance']['value']
-			puts duration = routes[0]['legs'][0]['duration']['value']
+			distance = routes[0]['legs'][0]['distance']['value']
+			duration = routes[0]['legs'][0]['duration']['value']
 
-			val = {'from_name': location['name'], 'to_name': location_in['name'], 'from_id': location['id'], 'to_id': location_in['id'], 'distance': distance, 'duration': duration }
+			val = { 'from_id': location['id'], 'to_id': location_in['id'], 'distance': distance, 'duration': duration }
 			matrix << val
 
-			val = {'from_name': location_in['name'], 'to_name': location['name'], 'from_id': location_in['id'], 'to_id': location['id'], 'distance': distance, 'duration': duration }
+			val = { 'from_id': location_in['id'], 'to_id': location['id'], 'distance': distance, 'duration': duration }
 			matrix << val
 
 			puts '.'
 		end
 	end
 
-	File.open(matrix_json, 'ab+') do |f|
+	File.open(matrix_json, 'w') do |f|
 		f.write JSON.pretty_generate(matrix)
 	end
 end
